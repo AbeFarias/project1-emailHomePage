f05f3ad7c8349c51249a05111d5b76fbae19e12c
Abraham Farias <afarias@Abrahams-MacBook-Air.local>
2019-07-10 16:56:24 -0700

    modal popup practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EmailItemComponent } from './email-item/email-item.component';
 import { EmailListComponent } from './email-list/email-list.component';
 import { TruncatePipe } from './truncate.pipe';
-import { PaginatorComponent } from './paginator/paginator.component';
 import { HighlightDirective } from './highlight.directive';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
@@ -17,14 +16,14 @@ import { MessageFormComponent } from './message-form/message-form.component';
     EmailItemComponent,
     EmailListComponent,
     TruncatePipe,
-    PaginatorComponent,
     HighlightDirective,
     LoginFormComponent,
     SignUpFormComponent,
     MessageFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [TruncatePipe],
   bootstrap: [AppComponent]
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
+  signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required),
+    });
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+      modal.style.display = "none";
+      }
+    }
+
   }
 
 }
