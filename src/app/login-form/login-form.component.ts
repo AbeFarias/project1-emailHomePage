3b54b42ef09cf2c4527b9a2acd1e29f82afe0cd8
Abraham Farias <afarias@Abrahams-MacBook-Air.local>
2019-07-16 13:00:46 -0700

    created formsfor login and messages

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  selector: "app-login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrls: ["./login-form.component.scss"]
 })
 export class LoginFormComponent implements OnInit {
   signupForm: FormGroup;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'password': new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required)
     });
 
     // Get the modal
@@ -26,23 +25,34 @@ export class LoginFormComponent implements OnInit {
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal 
+    // When the user clicks on the button, open the modal
     btn.onclick = function() {
       modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    //   modal.style.display = "none";
-    // }
+    };
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == modal) {
-      modal.style.display = "none";
+        modal.style.display = "none";
       }
-    }
+    };
 
+    // modal.showModal(id) {
+    //   $("#" + id).fadeIn('slow');
+    // };
+    
+    // modal.hideModal(id) {
+    //   $("#" + id).fadeOut('slow');
+    // };
   }
-
+  
+  // handleClick(): void {
+  //   var modal = document.getElementById("myModal");
+  //   this.modal.style.display = "none";
+  // }
+  
+  onSubmit() {
+    console.log(this.signupForm);
+    console.log(this.signupForm.value);
+  }
 }
--- src/app/message-form/message-form.component.ts
+++ src/app/message-form/message-form.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
-  selector: 'app-message-form',
-  templateUrl: './message-form.component.html',
-  styleUrls: ['./message-form.component.scss']
+  selector: "app-message-form",
+  templateUrl: "./message-form.component.html",
+  styleUrls: ["./message-form.component.scss"]
 })
 export class MessageFormComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      subject: new FormControl(null, Validators.required),
+      sendTo: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required)
+    });
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("msgBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+
+    // modal.showModal(id) {
+    //   $("#" + id).fadeIn('slow');
+    // };
+
+    // modal.hideModal(id) {
+    //   $("#" + id).fadeOut('slow');
+    // };
   }
 
+  // handleClick(): void {
+  //   var modal = document.getElementById("myModal");
+  //   this.modal.style.display = "none";
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    console.log(this.signupForm.value);
+  }
 }
